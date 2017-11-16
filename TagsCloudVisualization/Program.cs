e2ea6101752085b6426d5aaf8769b859f044f312
Vole1 <studying48@gmail.com>
2017-11-16 13:48:08 +0500

    Дополения

--- TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/Program.cs
@@ -18,18 +18,19 @@ namespace TagsCloudVisualization
 		private static IEnumerable<Size> GetSizes()
 		{
 			var rnd = new Random();
-			//var width = rnd.Next(150, 190);
-			//var height = rnd.Next(60, 70);
-			var height = rnd.Next(100, 150);
-			var width = rnd.Next(60, 70);
 
-			var coefficient = 0.995;
+			//var coefficient = 0.991;
+			var coefficient = 1;
 
 			var result = new Size[100];
 			for (var i = 0; i < 100; i++)
 			{
-				//var width = rnd.Next(30, 100);
-				//var height = rnd.Next(30, 100);
+				//var width = rnd.Next(80, 120);
+				//var height = rnd.Next(40, 60);
+
+				var width = rnd.Next(30, 100);
+				var height = rnd.Next(30, 100);
+
 				var currentWidth = (int)Math.Round(width * Math.Pow(coefficient, i));
 				var currentHeight = (int)Math.Round(height * Math.Pow(coefficient, i));
 				result[i] = new Size(currentWidth, currentHeight);
